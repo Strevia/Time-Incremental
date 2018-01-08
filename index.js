e46469229fb267616ffc4b340e1e59c4997e8343
Phantasy146 <32680459+Phantasy146@users.noreply.github.com>
2018-01-08 16:29:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ var minBuild = 0;
 var hou = 0;
 var houPrice = 1;
 var houBuild = 0;
-setInterval(function(){ 
-  secPerSec = secPerSec + minBuild*2/tick
+var time = 0;
+var day = 0;
+var dayPrice = 1;
+var dayBuild = 0;
+var tempTime = 0;
+setInterval(function(){
+  time++;
+  tempTime = time;
+  secPerSec = secPerSec + minBuild*2/tick;
   sec = sec + secPerSec*2/tick;
-  minBuild = minBuild + houBuild*2/tick
+  minBuild = minBuild + houBuild*2/tick;
+  houBuild = houBuild +dayBuild*2/tick;
+  minsPerMin = 60/((-1 + Math.sqrt(1-4*minBuild*-60))/(minBuild));
   document.getElementById("seconds").innerHTML = Math.round(sec);
   document.getElementById("secPerSecPrice").innerHTML = secPrice;
   document.getElementById("secPerSec").innerHTML = Math.round(secPerSec);
@@ -21,6 +30,37 @@ setInterval(function(){
   document.getElementById("hours").innerHTML = hou;
   document.getElementById("houPerSecPrice").innerHTML = houPrice;
   document.getElementById("houBuilds").innerHTML = Math.round(houBuild);
+  document.getElementById("days").innerHTML = day;
+  document.getElementById("dayPerSecPrice").innerHTML = dayPrice;
+  document.getElementById("dayBuilds").innerHTML = Math.round(dayBuild);
+  document.getElementById("dayTime").innerHTML = Math.floor(time/1728000);
+  document.getElementById("hourTime").innerHTML = Math.floor(time/72000 % 24);
+  document.getElementById("minTime").innerHTML = Math.floor(time/(1200) % 60);
+  document.getElementById("secondTime").innerHTML = Math.floor(time/20 % 60);
+  if (!(sec >= 30 || minBuild > 0 || min > 0)){
+    document.getElementById("numberMin").style.display = "none";
+    document.getElementById("minButton").style.display = "none";
+  }
+  else{
+    document.getElementById("numberMin").style.display = "block";
+    document.getElementById("minButton").style.display = "block";
+  }
+  if (!(min >= 30 || houBuild > 0 || hou > 0)){
+    document.getElementById("numberHour").style.display = "none";
+    document.getElementById("hourButton").style.display = "none";
+  }
+  else{
+    document.getElementById("numberHour").style.display = "block";
+    document.getElementById("hourButton").style.display = "block";
+  }
+  if (!(hou >= 24 || dayBuild > 0 || day > 0)){
+    document.getElementById("numberDay").style.display = "none";
+    document.getElementById("dayButton").style.display = "none";
+  }
+  else{
+    document.getElementById("numberDay").style.display = "block";
+    document.getElementById("dayButton").style.display = "block";
+  }
   if (sec >= 60){
     sec = 1;
     secPrice = 1;
@@ -32,5 +72,11 @@ setInterval(function(){
     minPrice = 1;
     minBuild = 0;
     hou = hou + 1
+  }
+  if (hou >= 24){
+    hou = 0;
+    houPrice = 1;
+    houBuild = 0;
+    day++;
   }
 }, tick);
