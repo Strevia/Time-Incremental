9317f79f0b4e28863779bf1eec816176d7740bdc
Phantasy146 <32680459+Phantasy146@users.noreply.github.com>
2018-01-09 18:01:58 -0500

    Add files via upload
    Added incentive to buy singular seconds and days. Each manually bought time accelerator gives an x2 to each manually bought increase in second per second. Each manually bought hour build extends your possible seconds by 60 and gives you extra minutes based on how far it extended.

--- index.js
+++ index.js
@@ -2,22 +2,48 @@ function getCookieValue(a) {
     var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
     return b ? b.pop() : '';
 }
+  var sec;
+var secPerSec;
+var secPrice;
+var tick;
+var min;
+var minPrice;
+var minBuild;
+var hou;
+var houPrice;
+var houBuild;
+var time;
+var day;
+var dayPrice;
+var dayBuild;
+var manualHou;
+var secondGain;
+var accelDouble;
+var manualMin;
+var manualSecIncrease;
+function initialize(){
+sec = 1;
+secPerSec = 0;
+secPrice = 1;
+tick = 1000/20;
+min = 0;
+minPrice = 1;
+minBuild = 0;
+hou = 0;
+houPrice = 1;
+houBuild = 0;
+time = 0;
+day = 0;
+dayPrice = 1;
+dayBuild = 0;
+manualHou = 0
+secondGain = true;
+accelDouble = true;
+manualMin = 0;
+}
 var x = getCookieValue("everything").split(',');
 if(x.length === 1){
-var sec = 1;
-var secPerSec = 0;
-var secPrice = 1;
-var tick = 1000/20;
-var min = 0;
-var minPrice = 1;
-var minBuild = 0;
-var hou = 0;
-var houPrice = 1;
-var houBuild = 0;
-var time = 0;
-var day = 0;
-var dayPrice = 1;
-var dayBuild = 0;
+  initialize()
 }
 else {
   var sec = parseFloat(x[0])
@@ -34,45 +60,65 @@ else {
   var day = parseFloat(x[10]);
   var dayPrice = parseFloat(x[11]);
   var dayBuild = parseFloat(x[12]);
+  var manualHou = parseInt(x[13]);
+  var secondGain = x[14] == 'true';
+  var accelDouble = x[15] == 'true';
+  var manualMin = x[16];
 }
 setInterval(function(){
-  document.cookie = "everything="+[sec, secPerSec,secPrice,min,minPrice,minBuild,hou,houPrice,houBuild,time,day,dayPrice,dayBuild].toString();
+  document.cookie = "everything="+[sec, secPerSec,secPrice,min,minPrice,minBuild,hou,houPrice,houBuild,time,day,dayPrice,dayBuild,manualHou, secondGain, accelDouble, manualMin].toString();
   time++;
   secPerSec = secPerSec + minBuild*2/tick;
   sec = sec + secPerSec*2/tick;
   minBuild = minBuild + houBuild*2/tick;
   houBuild = houBuild +dayBuild*2/tick;
+  manualSecIncrease = Math.floor((accelDouble+1)**(manualMin));
   document.getElementById("seconds").innerHTML = Math.round(sec);
   document.getElementById("secPerSecPrice").innerHTML = secPrice;
   document.getElementById("secPerSec").innerHTML = Math.round(secPerSec);
-  document.getElementById("minutes").innerHTML = min;
-  document.getElementById("minPerSecPrice").innerHTML = minPrice;
+  document.getElementById("minutes").innerHTML = Math.round(min);
+  document.getElementById("minPerSecPrice").innerHTML = Math.round(minPrice);
   document.getElementById("minBuilds").innerHTML = Math.round(minBuild);
-  document.getElementById("hours").innerHTML = hou;
-  document.getElementById("houPerSecPrice").innerHTML = houPrice;
+  document.getElementById("hours").innerHTML = Math.round(hou);
+  document.getElementById("houPerSecPrice").innerHTML = Math.round(houPrice);
   document.getElementById("houBuilds").innerHTML = Math.round(houBuild);
-  document.getElementById("days").innerHTML = day;
-  document.getElementById("dayPerSecPrice").innerHTML = dayPrice;
+  document.getElementById("days").innerHTML = Math.round(day);
+  document.getElementById("dayPerSecPrice").innerHTML = Math.round(dayPrice);
   document.getElementById("dayBuilds").innerHTML = Math.round(dayBuild);
   document.getElementById("dayTime").innerHTML = Math.floor(time/1728000);
   document.getElementById("hourTime").innerHTML = Math.floor(time/72000 % 24);
   document.getElementById("minTime").innerHTML = Math.floor(time/(1200) % 60);
   document.getElementById("secondTime").innerHTML = Math.floor(time/20 % 60);
+  document.getElementById("manualSeconds").innerHTML = manualSecIncrease;
   if (!(sec >= 30 || minBuild > 0 || min > 0)){
     document.getElementById("numberMin").style.display = "none";
     document.getElementById("minButton").style.display = "none";
+    //document.getElementById("minUpgrade").style.display = "none";
   }
   else{
     document.getElementById("numberMin").style.display = "block";
     document.getElementById("minButton").style.display = "block";
+    if(!accelDouble){
+      //document.getElementById("minUpgrade").style.display = "block"
+    }
+    else {
+      //document.getElementById("minUpgrade").style.display = "none"
+    }
   }
   if (!(min >= 30 || houBuild > 0 || hou > 0)){
     document.getElementById("numberHour").style.display = "none";
     document.getElementById("hourButton").style.display = "none";
+    //document.getElementById("breakSeconds").style.display = "none";
   }
   else{
     document.getElementById("numberHour").style.display = "block";
     document.getElementById("hourButton").style.display = "block";
+    if (!secondGain){
+      //document.getElementById("breakSeconds").style.display = "block";
+    }
+    else{
+      //document.getElementById("breakSeconds").style.display = "none";
+    }
   }
   if (!(hou >= 24 || dayBuild > 0 || day > 0)){
     document.getElementById("numberDay").style.display = "none";
@@ -82,17 +128,18 @@ setInterval(function(){
     document.getElementById("numberDay").style.display = "block";
     document.getElementById("dayButton").style.display = "block";
   }
-  if (sec >= 60){
+  if (sec >= (60+manualHou*secondGain*60)){
     sec = 1;
     secPrice = 1;
     secPerSec = 0;
-    min = min + 1;
+    min = min + manualHou*secondGain + 1;
   }
   if (min >= 60){
     min = 0
     minPrice = 1;
     minBuild = 0;
-    hou = hou + 1
+    hou = hou + 1;
+    manualMin = 0;
   }
   if (hou >= 24){
     hou = 0;
